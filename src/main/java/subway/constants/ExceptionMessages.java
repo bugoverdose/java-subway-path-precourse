295d0d92962270965eb8aa727275cd75f5024302
Jeong Satchel <bugod96@gmail.com>
2021-12-17 17:32:07 +0900

    feat: 경로 조회 시 출발역과 도착역의 입력값이 같으면 에러를 출력하고 다시 입력받기

--- src/main/java/subway/constants/ExceptionMessages.java
+++ src/main/java/subway/constants/ExceptionMessages.java
@@ -4,4 +4,5 @@ public class ExceptionMessages {
 
     public static String ERROR = "[ERROR] ";
     public static String NOT_EXISTING_STATION_NAME_EXCEPTION = ERROR + "존재하지 않는 역 이름입니다.\n";
+    public static String SAME_STATION_NAME_INPUT_EXCEPTION = ERROR + "출발역과 도착역이 동일합니다.\n";
 }
--- src/main/java/subway/utils/InputValidator.java
+++ src/main/java/subway/utils/InputValidator.java
@@ -2,13 +2,20 @@ package subway.utils;
 
 import subway.domain.StationRepository;
 
-import static subway.constants.ExceptionMessages.NOT_EXISTING_STATION_NAME_EXCEPTION;
+import static subway.constants.ExceptionMessages.*;
 
 public class InputValidator {
 
     public static void validateFindPathInput(String startStationName, String endStationName) {
         validateExistingStation(startStationName);
         validateExistingStation(endStationName);
+        validateNotSameStation(startStationName, endStationName);
+    }
+
+    public static void validateNotSameStation (String startStationName, String endStationName) {
+        if (startStationName.equals(endStationName)){
+            throw new IllegalArgumentException(SAME_STATION_NAME_INPUT_EXCEPTION);
+        }
     }
 
     public static void validateExistingStation (String stationName) {
