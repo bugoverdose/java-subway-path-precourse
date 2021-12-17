99c8478a7fd712f933890f785a1894a6e6de70c8
Jeong Satchel <bugod96@gmail.com>
2021-12-17 13:03:36 +0900

    feat(PathService): 경로 조회 화면에서 메인 화면으로 돌아갈 수 있다

--- src/main/java/subway/service/PathService.java
+++ src/main/java/subway/service/PathService.java
@@ -6,5 +6,17 @@ public class PathService {
 
     public void run() {
         printPathViewMenu();
+        while (true) {
+            String userChoice = requestUserChoiceInput();
+            if (userChoice.equals("1")) {
+                break;
+            }
+            if (userChoice.equals("2")) {
+                break;
+            }
+            if (userChoice.equals("B")) {
+                break;
+            }
+        }
     }
 }
